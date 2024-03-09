1bb5fd208576a42fb78a23fba7cc01334cdee963
MZ <zajkowskimarkusmartin@gmail.com>
2024-03-09 17:40:20 -0500

    test(api-tests): :rocket: added tests

--- tests/GeometricSequenceTests/FibonacciControllerTest.php
+++ tests/GeometricSequenceTests/FibonacciControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Tests\Controller;
+namespace App\tests\FibonacciTests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use App\Service\FibonacciService\FibonacciService;
--- tests/GeometricSequenceTests/GeometricSequenceTest.php
+++ tests/GeometricSequenceTests/GeometricSequenceTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Tests\Controller;
+namespace App\tests\GeometricSequenceTests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use App\Controller\GeometricController\GeometricController;
